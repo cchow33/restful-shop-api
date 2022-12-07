da7fc848542fcdd66e91caf10e1bdb14d48d6290
Carmen Chow <carmendevelops@gmail.com>
2022-12-07 13:16:30 -0500

    set up default logging and error handling

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// 1. Implement route for GET requests (localhost:3000/orders/)
+// 1. Implement route for GET requests to /orders (localhost:3000/orders/)
 router.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'Orders were fetched'
--- app.js
+++ app.js
@@ -1,23 +1,35 @@
-// import express library
+// 1. import express library
 const express = require('express');
 const app = express();
+// import morgan (HTTP request level middleware) to log requests and how long the requests take. It calls the 'next' function 
+const morgan = require('morgan');
+
+// 3. Routes to handle requests
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-
-
-// Set up middleware with use();
+// 2. Set up middleware with use() to forward requests for /products or /orders.
 // Anything starting with the first argument '/products', will be handled by 'productRoutes' and forwarded to the product page:
-
+app.use(morgan('dev'));
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
 
-// app.use((req, res, next) => {
-//   res.status(200).json({
-//     message: 'OMG, it STILL works!'
-//   });
-// });
+// Handle error requests 
+app.use((req, res, next) => { 
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+});
 
 module.exports = app;
 
