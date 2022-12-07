b3d6c5b07aaa8ba5cac6bc57b146c0a44104b783
Carmen Chow <carmendevelops@gmail.com>
2022-12-07 11:25:41 -0500

    handle POST and GET routes

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ const app = express();
 // Set up middleware with use();
 app.use((req, res, next) => {
   res.status(200).json({
-    message: 'iT works!'
+    message: 'It STILL works!'
   });
-})
+});
 
 module.exports = app;
+
+
+// Set up middleware with use();
+// Anything starting with the first argument '/products', will be handled by 'productRoutes' and forwarded to the product page:
+// app.use('/products', productRoutes);
+
+// module.exports = app;
