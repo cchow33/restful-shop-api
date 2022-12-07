07e61cee023ca738fd393fdc65542e0ed036b701
Carmen Chow <carmendevelops@gmail.com>
2022-12-07 11:55:54 -0500

    adding routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,1 +1,52 @@
-const express = require('express')
+const express = require('express');
+const router = express.Router();
+
+// 1. Implement a GET route using the get() method to handle GET requests. First argument is the url, second argument is the handler.
+router.get('/', (req, res, next) => { 
+  res.status(200).json({
+    // return an object with the message property:
+      message: 'Handling GET requests to /products'
+  });
+});
+
+// 2. Implement a POST route using the post() method to handle POST requests.
+router.post('/', (req, res, next) => { 
+  res.status(200).json({
+    // return an object with the message property:
+      message: 'Handling POST requests to /products'
+  });
+});
+
+// 3. Implement a GET route for the productID
+router.get('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  if (id === 'special') {
+    res.status(200).json({
+      message: 'You discovered the special product id',
+      id: id
+    });
+  } else {
+      res.status(200).json({
+        message: 'You passed an id'
+      });
+    }
+});
+
+// 4. Implement a PATCH route to modify a resource
+router.patch('/:productId', (req, res, next) => {
+  // don't need to add 'return' unless there's another response after the response
+  res.status(200).json({
+    message: 'Updated a product!'
+  });
+});
+
+// 5. Implement a DELETE route to delete a resource
+router.delete('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Deleted a product!'
+  });
+});
+
+
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 // import express library
 const express = require('express');
 const app = express();
+const productRoutes = require('./api/routes/products');
 
 // Set up middleware with use();
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'It STILL works!'
-  });
-});
+// Anything starting with the first argument '/products', will be handled by 'productRoutes' and forwarded to the product page:
+
+app.use('/products', productRoutes);
+
+
+// app.use((req, res, next) => {
+//   res.status(200).json({
+//     message: 'OMG, it STILL works!'
+//   });
+// });
 
 module.exports = app;
 
 
-// Set up middleware with use();
-// Anything starting with the first argument '/products', will be handled by 'productRoutes' and forwarded to the product page:
-// app.use('/products', productRoutes);
-
-// module.exports = app;
